2242aa52df894138fcbc8651aeb2d201c7bcab4c
MTXayko <matias.marin.cariaga@alumnos.sip.cl>
2022-04-12 11:07:03 -0400

    cambio

--- Marin_MatiasJSPrediccion.js
+++ Marin_MatiasJSPrediccion.js
@@ -3,7 +3,7 @@
 function myBirthYearFunc(){
     console.log("Nací en " + 1980);
 }
-
+// se imprimira el resultado dentro del parentesis dando el siguiente resultado: "Nací en 1980"
 
 //Predicción 2: si necesitáramos enviar un año de nacimiento hacia la función, le diríamos a la función "oye, te enviaremos una variable llamada EntradaAñoNacimiento". Lo hacemos agregando el nombre de la variable entre paréntesis. Mira a continuación un ejemplo. 
 
@@ -12,7 +12,7 @@ function myBirthYearFunc(){
 function myBirthYearFunc(EntradaAñoNacimiento){
     console.log("Nací en " + EntradaAñoNacimiento);
 }
-
+// lo mismo que el anterior, pero la diferencia es que uno debe poner el año en los entreparentesis para asi ejecutarlo y dar resultado.
 
 //Predicción 3: Intentémoslo otra vez. Si var num1 = 10 y var num2 = 20, ¿qué indicaría console.log?
 
@@ -23,6 +23,9 @@ function add(num1, num2){
     var sum = num1 + num2;
     console.log(sum);
 }
+
+// se suman actualizan los numeros establecidos en cada linea y luego se suman.
+
 /*
 git config --global user.name "MTXayko"
 git config --global user.email "matias.marin.cariaga@alumnos.sip.cl"
